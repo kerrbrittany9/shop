4b02dc4e3e0178096e6e2ed87656738d66c55d12
brittany kerr <kerrbrittany9@gmail.com>
2017-10-03 11:37:29 -0700

    add actiontypes file

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,5 @@
+export const CART_ADD: "CART_ADD";
+export const CART_REMOVE: "CART_REMOVE",
+export const CART_VISIBLE: "CART_VISIBLE",
+export const SET_SELECTED: "SET_SELECTED",
+export const RECEIVE_DATA: "RECEIVE_DATA" 
---
+++ src/reducer/CartReducer.js
@@ -0,0 +1,15 @@
+import constants from "./../constants";
+
+const { defaultState, types } = constants;
+
+
+// Create dispatcher instance
+const cartReducer = (state = defaultState, action) => {
+  let newState;
+  switch (action.type) {
+    case types.
+
+  }
+}
+
+export default cartReducer;
